dd583a2efff8fc1d2c44791a5566f69733b99423
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-07-07 00:41:52 +0200

    add alerts test

--- src/main/java/com/demoqa/pages/AlertsPage.java
+++ src/main/java/com/demoqa/pages/AlertsPage.java
@@ -1,29 +1,91 @@
 package com.demoqa.pages;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+
+import java.time.Duration;
 
 public class AlertsPage extends BasePage {
     public AlertsPage(WebDriver wd) {
         super(wd);
     }
 
+    public boolean isAlertPresent() {
+
+        Alert alert = new WebDriverWait(wd, Duration.ofSeconds(6))
+                .until(ExpectedConditions.alertIsPresent());
+        if (alert == null) {
+            return false;
+        } else {
+            wd.switchTo().alert();
+            alert.accept();
+            return true;
+        }
+    }
+
     @FindBy(id = "promtButton")
     WebElement promtButton;
+
     public AlertsPage sendMessageToAlert(String message) {
-        clickWithJSExecutor(promtButton,0,300);
-        if( message!= null) {
+        clickWithJSExecutor(promtButton, 0, 300);
+        if (message != null) {
             wd.switchTo().alert().sendKeys(message);
             wd.switchTo().alert().accept();
         }
         return this;
     }
-@FindBy(id="promptResult")
-WebElement promptResult;
+
+    @FindBy(id = "promptResult")
+    WebElement promptResult;
+
     public AlertsPage assertMessage(String message) {
         Assert.assertTrue(promptResult.getText().contains(message));
         return this;
+    }
+
+    @FindBy(id = "alertButton")
+    WebElement alertButton;
+
+    public AlertsPage clickAllertButton() {
+        clickWithJSExecutor(alertButton, 0, 300);
+        return this;
+    }
+
+    @FindBy(id = "timerAlertButton")
+    WebElement timerAlertButton;
+
+    public AlertsPage clickTimerAlertButton() {
+        clickWithJSExecutor(timerAlertButton, 0, 300);
+        Assert.assertTrue(isAlertPresent());
+        return this;
+    }
+
+    @FindBy(id = "confirmButton")
+    WebElement confirmButton;
+    @FindBy(id = "confirmResult")
+    WebElement confirmResult;
+
+    public AlertsPage onButtonClickConfirmBox() {
+        clickWithJSExecutor(confirmButton, 0, 300);
+        return this;
+    }
+
+    public boolean isTextCorrect() {
+        String text = confirmResult.getText();
+        if (text.equalsIgnoreCase("You selected Ok")) {
+            return true;
+        } else return false;
+    }
+
+    public boolean isTextCancelCorrect() {
+        String text = confirmResult.getText();
+        if (text.equalsIgnoreCase("You selected Cancel")) {
+            return true;
+        } else return false;
     }
 }
--- src/test/java/com/demoqa/tests/AlertsTests.java
+++ src/test/java/com/demoqa/tests/AlertsTests.java
@@ -3,10 +3,12 @@ package com.demoqa.tests;
 import com.demoqa.pages.AlertsPage;
 import com.demoqa.pages.HomePage;
 import com.demoqa.pages.SidePanel;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class AlertsTests extends TestBase{
+
+public class AlertsTests extends TestBase {
     @BeforeMethod
     public void precondition() {
         new HomePage(wd).getAlertsFrameWindows();
@@ -18,4 +20,30 @@ public class AlertsTests extends TestBase{
         new AlertsPage(wd).sendMessageToAlert("Hello world")
                 .assertMessage("Hello world");
     }
+
+    @Test
+    public void clickButtonToSeeAlert() {
+        new AlertsPage(wd).clickAllertButton();
+        wd.switchTo().alert().accept();
+    }
+
+    @Test
+    public void OnButtonClick5Sec() {
+        new AlertsPage(wd).clickTimerAlertButton();
+
+    }
+
+    @Test
+    public void onButtonClickConfirmBoxTest() {
+        AlertsPage AP = new AlertsPage(wd).onButtonClickConfirmBox();
+        wd.switchTo().alert().accept();
+        Assert.assertTrue(AP.isTextCorrect());
+    }
+    @Test
+    public void onButtonClickCancelBoxTest() {
+        AlertsPage AP = new AlertsPage(wd).onButtonClickConfirmBox();
+        wd.switchTo().alert().dismiss();
+        Assert.assertTrue(AP.isTextCancelCorrect());
+    }
+
 }
